1e7948525e88b1b2bec119a8b6c51863cb969c81
AverageHippo <mitchell.benkic@7shifts.com>
2020-03-20 13:20:17 -0600

    PRIME-412 fixed renaming errors.

--- signinwithapplebutton/src/main/java/com/willowtreeapps/signinwithapplebutton/SignInWithAppleCallback.kt
+++ signinwithapplebutton/src/main/java/com/willowtreeapps/signinwithapplebutton/SignInWithAppleCallback.kt
@@ -12,7 +12,7 @@ interface SignInWithAppleCallback {
 internal fun SignInWithAppleCallback.toFunction(): (SignInWithAppleResult) -> Unit =
     { result ->
         when (result) {
-            is SignInWithAppleResult.Success -> onSignInWithAppleSuccess(result.authorizationCode)
+            is SignInWithAppleResult.Success -> onSignInWithAppleSuccess(result.idToken)
             is SignInWithAppleResult.Failure -> onSignInWithAppleFailure(result.error)
             is SignInWithAppleResult.Cancel -> onSignInWithAppleCancel()
         }
